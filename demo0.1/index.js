2debee523ddc8d6f7e99ca16a0a0a572d8856f7e
alwxkxk <alwxkxk@gmail.com>
2019-03-16 17:49:52 +0800

    添加demo0.1

---
+++ demo0.1/index.js
@@ -0,0 +1,50 @@
+let http = require('http');
+let server = http.createServer((request, response)=>{
+  console.log(request)
+  console.log(response)
+  response.end('123');
+});
+
+server.listen(8000);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}

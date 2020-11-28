2e4f328e1314c5c04da06ff22aee62889a6aca58
alwxkxk <alwxkxk@gmail.com>
2020-11-28 10:04:38 +0800

    fix: 修复微信小程序URL配置错误。

--- demo2/wxapp/app.js
+++ demo2/wxapp/app.js
@@ -8,8 +8,9 @@ App({
   globalData: {
     // 初期调试时可以设置不检验合法域名，否则调用API时需要设置小程序的request、socket合法域名
     // 本机调试需要本机运行demo2，然后将API指向127.0.0.1
-    websocketURL: localDev ? 'ws://127.0.0.1:8002' : 'wss://42.192.168.165',
-    requestHost: localDev ? 'http://127.0.0.1:8002' : 'https://42.192.168.165',
+    // 网上调试必须使用ssl，所以我额外申请了sh.scaugreen.cn域名并配置了SSL证书。
+    websocketURL: localDev ? 'ws://127.0.0.1:8002' : 'wss://sh.scaugreen.cn',
+    requestHost: localDev ? 'http://127.0.0.1:8002' : 'https://sh.scaugreen.cn',
     equipmentId: '123456'
   }
 })

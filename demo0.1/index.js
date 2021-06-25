e0897843fa1e5c9bedb2c103659509b7b4f7d909
alwxkxk <592364224@qq.com>
2021-06-25 20:11:37 +0800

    fix: 修复 demo0.1 报错时的异常。

--- demo0.1/index.js
+++ demo0.1/index.js
@@ -141,24 +141,9 @@ function onError(error) {
   if (error.syscall !== 'listen') {
     throw error;
   }
+  console.error('demo0.1服务运行报错，有可能是重复运行导致的端口冲突。')
+  console.error(error)
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
 }
 
 /**
